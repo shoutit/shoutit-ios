2b1658dd07796bddd35a55ae16da950fe174f1b1
Hitesh Sondhi <hitesh@writingminds.com>
2015-11-14 18:18:11 +0530

    Added CreateShout Controller

---
+++ shoutit-iphone/ViewModel/tabs/create/SHCreateShoutViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  SHCreateShoutViewModel.swift
+//  shoutit-iphone
+//
+//  Created by Hitesh Sondhi on 14/11/15.
+//  Copyright © 2015 Shoutit. All rights reserved.
+//
+
+import UIKit
+
+class SHCreateShoutViewModel: NSObject, TableViewControllerModelProtocol, UITableViewDelegate, UITableViewDataSource {
+
+    private let viewController: SHCreateShoutTableViewController
+    
+    required init(viewController: SHCreateShoutTableViewController) {
+        self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        
+    }
+    
+    func viewWillAppear() {
+        
+    }
+    
+    func viewDidAppear() {
+        
+    }
+    
+    func viewWillDisappear() {
+        
+    }
+    
+    func viewDidDisappear() {
+        
+    }
+    
+    func destroy() {
+        
+    }
+    
+    // MARK - TableView Delegate
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return Enums.LocationSections.TableViewSectionCount.rawValue
+    }
+    
+}
