aacc7639ab8b7f939f47d7b6d86b15e03c8a2891
Vishal Thakur <vishalthakurnhn@gmail.com>
2015-12-09 15:48:42 +0530

    Profile Tab Added

---
+++ shoutit-iphone/ViewModel/profile/collectionProfile/SHEditProfileTableViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SHEditProfileTableViewModel.swift
+//  shoutit-iphone
+//
+//  Created by Vishal Thakur on 09/12/15.
+//  Copyright © 2015 Shoutit. All rights reserved.
+//
+
+import UIKit
+
+class SHEditProfileTableViewModel: NSObject {
+
+}
