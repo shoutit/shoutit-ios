b1ae8ebdaa4a3c766d1720002c0fc5d2fa46045b
piotrbernad <piotr.bernad@appunite.com>
2016-02-11 11:14:57 +0100

    Send User Location to API[WIP]

--- shoutit-iphone/Util/LocationManager.swift
+++ shoutit-iphone/Util/LocationManager.swift
@@ -7,37 +7,80 @@
 //
 
 import Foundation
+import CoreLocation
 
-class LocationManager: NSObject {
+final class LocationManager: NSObject {
     
     static let sharedInstance = LocationManager()
     
     private let locationManager = CLLocationManager()
-    private (set) var currentLocation = CLLocation()
+    private (set) var currentLocation = CLLocation() {
+        didSet {
+            updateUserCoordinates(currentLocation.coordinate)
+        }
+    }
     
     private override init() {
         super.init()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
-        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.authorizationStatus() != .AuthorizedWhenInUse {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
         locationManager.distanceFilter = 5000 // 5000m to update location
+        
+        startMonitoringSignificantLocationChanges()
+    }
+    
+    deinit {
+        stopMonitoringSignificantLocationChanges()
     }
     
     func startUpdatingLocation() {
-        locationManager.startMonitoringSignificantLocationChanges()
+        locationManager.startUpdatingLocation()
     }
     
     func stopUpdatingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func startMonitoringSignificantLocationChanges() {
+        locationManager.startMonitoringSignificantLocationChanges()
+    }
+    
+    func stopMonitoringSignificantLocationChanges() {
         locationManager.stopMonitoringSignificantLocationChanges()
     }
     
+    func updateUserCoordinates(coordinates: CLLocationCoordinate2D) {
+        guard let username = Account.sharedInstance.user?.username else {
+            return
+        }
+        APILocationService.updateLocation(username, coordinates: coordinates) { (result) -> Void in
+            
+        }
+    }
     
 }
 
 extension LocationManager: CLLocationManagerDelegate {
     
-    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let lastLocation = locations.first else {
+            return
+        }
         
-        currentLocation = newLocation
+        if CLLocationCoordinate2DIsValid(lastLocation.coordinate) {
+            currentLocation = lastLocation
+            stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        if CLLocationCoordinate2DIsValid(newLocation.coordinate) {
+            currentLocation = newLocation
+        }
     }
 }
