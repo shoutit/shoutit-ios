fe014e678b7eb48422b4f0ea0a5d8918b66d6587
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-02-12 11:34:15 +0100

    [WIP] my profile collection view controller

---
+++ shoutit-iphone/Controllers/ProfileCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCollectionViewController.swift
+//  shoutit-iphone
+//
+//  Created by Łukasz Kasperek on 11.02.2016.
+//  Copyright © 2016 Shoutit. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewController: UICollectionViewController {
+
+}
