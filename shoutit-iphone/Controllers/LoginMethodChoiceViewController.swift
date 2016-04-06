4cd969cb36837c5904ce608cb9cbe2174fc7a63d
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-04-06 16:45:00 +0200

    #154 google sign in

--- shoutit-iphone/Controllers/LoginMethodChoiceViewController.swift
+++ shoutit-iphone/Controllers/LoginMethodChoiceViewController.swift
@@ -41,13 +41,7 @@ final class LoginMethodChoiceViewController: UIViewController {
         navigationItem.titleView = UIImageView(image: UIImage.navBarLogoImage())
         
         // configure google client
-        GIDSignIn.sharedInstance().clientID = Constants.Google.clientID
-        GIDSignIn.sharedInstance().serverClientID = Constants.Google.serverClientID
-        GIDSignIn.sharedInstance().allowsSignInWithBrowser = false
-        GIDSignIn.sharedInstance().shouldFetchBasicProfile = true
-        GIDSignIn.sharedInstance().allowsSignInWithWebView = true
         GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().scopes = ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/userinfo.email"]
         
         // setup
         setupRX()
