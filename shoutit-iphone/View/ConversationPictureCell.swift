d4bbb7287ea386ab8755e796e671b29ac37e0020
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-04-12 18:19:08 +0200

    uncomment

--- shoutit-iphone/View/ConversationPictureCell.swift
+++ shoutit-iphone/View/ConversationPictureCell.swift
@@ -16,45 +16,45 @@ final class ConversationPictureCell: UITableViewCell, ConversationCell {
     @IBOutlet weak var timeLabel: UILabel?
     @IBOutlet weak var pictureImageView: UIImageView!
     
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        self.selectionStyle = .None
-//    }
-//    
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        unHideImageView()
-//        pictureImageView.image = nil
-//    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .None
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        unHideImageView()
+        pictureImageView.image = nil
+    }
     
     func bindWithMessage(message: Message, previousMessage: Message?) {
-//        if let imgview = avatarImageView {
-//            setImageWith(imgview, message: message)
-//        }
-//        
-//        timeLabel?.text = DateFormatters.sharedInstance.hourStringFromEpoch(message.createdAt)
-//        
-//        if message.isSameSenderAs(previousMessage) {
-//            hideImageView()
-//        }
-//        
-//        activityIndicator?.startAnimating()
-//        activityIndicator?.hidden = false
-//        
-//        setThumbMessage(message)
+        if let imgview = avatarImageView {
+            setImageWith(imgview, message: message)
+        }
+        
+        timeLabel?.text = DateFormatters.sharedInstance.hourStringFromEpoch(message.createdAt)
+        
+        if message.isSameSenderAs(previousMessage) {
+            hideImageView()
+        }
+        
+        activityIndicator?.startAnimating()
+        activityIndicator?.hidden = false
+        
+        setThumbMessage(message)
     }
     
     func setThumbMessage(message: Message) {
-//        guard let imagePath = message.attachment()?.imagePath(), let url = NSURL(string: imagePath) else {
-//            activityIndicator?.stopAnimating()
-//            activityIndicator?.hidden = true
-//            pictureImageView.image = UIImage.shoutsPlaceholderImage()
-//            return
-//        }
-//        
-//        pictureImageView.sh_setImageWithURL(url, placeholderImage: UIImage.shoutsPlaceholderImage(), optionsInfo: nil) {[weak self] (image, error, cacheType, imageURL) in
-//            self?.activityIndicator?.stopAnimating()
-//            self?.activityIndicator?.hidden = true
-//        }
+        guard let imagePath = message.attachment()?.imagePath(), let url = NSURL(string: imagePath) else {
+            activityIndicator?.stopAnimating()
+            activityIndicator?.hidden = true
+            pictureImageView.image = UIImage.shoutsPlaceholderImage()
+            return
+        }
+        
+        pictureImageView.sh_setImageWithURL(url, placeholderImage: UIImage.shoutsPlaceholderImage(), optionsInfo: nil) {[weak self] (image, error, cacheType, imageURL) in
+            self?.activityIndicator?.stopAnimating()
+            self?.activityIndicator?.hidden = true
+        }
     }
 }
