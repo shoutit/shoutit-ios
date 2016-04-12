fbf5d67a5fc4b12ddd07286a58178092854ed30a
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-04-12 17:46:35 +0200

    comment out code from ConversationImageCell

--- shoutit-iphone/View/ConversationImageCell.swift
+++ shoutit-iphone/View/ConversationImageCell.swift
@@ -16,45 +16,45 @@ final class ConversationImageCell: UITableViewCell, ConversationCell {
     @IBOutlet weak var timeLabel: UILabel?
     @IBOutlet weak var pictureImageView: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.selectionStyle = .None
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        unHideImageView()
-        pictureImageView.image = nil
-    }
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        self.selectionStyle = .None
+//    }
+//    
+//    override func prepareForReuse() {
+//        super.prepareForReuse()
+//        unHideImageView()
+//        pictureImageView.image = nil
+//    }
  
     func bindWithMessage(message: Message, previousMessage: Message?) {
-        if let imgview = avatarImageView {
-            setImageWith(imgview, message: message)
-        }
-        
-        timeLabel?.text = DateFormatters.sharedInstance.hourStringFromEpoch(message.createdAt)
-        
-        if message.isSameSenderAs(previousMessage) {
-            hideImageView()
-        }
-        
-        activityIndicator?.startAnimating()
-        activityIndicator?.hidden = false
-        
-        setThumbMessage(message)
+//        if let imgview = avatarImageView {
+//            setImageWith(imgview, message: message)
+//        }
+//        
+//        timeLabel?.text = DateFormatters.sharedInstance.hourStringFromEpoch(message.createdAt)
+//        
+//        if message.isSameSenderAs(previousMessage) {
+//            hideImageView()
+//        }
+//        
+//        activityIndicator?.startAnimating()
+//        activityIndicator?.hidden = false
+//        
+//        setThumbMessage(message)
     }
     
     func setThumbMessage(message: Message) {
-        guard let imagePath = message.attachment()?.imagePath(), url = NSURL(string: imagePath) else {
-            self.activityIndicator?.stopAnimating()
-            self.activityIndicator?.hidden = true
-            self.pictureImageView.image = UIImage.shoutsPlaceholderImage()
-            return
-        }
-        
-        self.pictureImageView.sh_setImageWithURL(url, placeholderImage: UIImage.shoutsPlaceholderImage(), optionsInfo: nil) { (image, error, cacheType, imageURL) in
-            self.activityIndicator?.stopAnimating()
-            self.activityIndicator?.hidden = true
-        }
+//        guard let imagePath = message.attachment()?.imagePath(), url = NSURL(string: imagePath) else {
+//            self.activityIndicator?.stopAnimating()
+//            self.activityIndicator?.hidden = true
+//            self.pictureImageView.image = UIImage.shoutsPlaceholderImage()
+//            return
+//        }
+//        
+//        self.pictureImageView.sh_setImageWithURL(url, placeholderImage: UIImage.shoutsPlaceholderImage(), optionsInfo: nil) { (image, error, cacheType, imageURL) in
+//            self.activityIndicator?.stopAnimating()
+//            self.activityIndicator?.hidden = true
+//        }
     }
 }
