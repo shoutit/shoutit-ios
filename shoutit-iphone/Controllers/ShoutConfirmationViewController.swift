cb2c60aec683c59a3c832c2ff76ec1502337739e
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-04-18 15:51:40 +0200

    add shout title to confirmation screen

--- shoutit-iphone/Controllers/ShoutConfirmationViewController.swift
+++ shoutit-iphone/Controllers/ShoutConfirmationViewController.swift
@@ -19,11 +19,20 @@ class ShoutConfirmationViewController: UIViewController {
         super.viewDidLoad()
         
         if shout.type()! == ShoutType.Offer {
-            createAnotherButton.setTitle("Create another Offer", forState: .Normal)
-            descriptionlabel.text = NSLocalizedString("Your offer will appear on Shoutit soon.", comment:"")
+            createAnotherButton.setTitle(NSLocalizedString("Create another Offer", comment: ""), forState: .Normal)
+            if let title = shout.title {
+                descriptionlabel.text = NSLocalizedString("Your offer \"\(title)\" will appear on Shoutit soon.", comment:"")
+            } else {
+                descriptionlabel.text = NSLocalizedString("Your offer will appear on Shoutit soon.", comment:"")
+            }
+            
         } else {
-            createAnotherButton.setTitle("Create another Request", forState: .Normal)
-            descriptionlabel.text = NSLocalizedString("Your request will appear on Shoutit soon.", comment:"")
+            createAnotherButton.setTitle(NSLocalizedString("Create another Request", comment: ""), forState: .Normal)
+            if let title = shout.title {
+                descriptionlabel.text = NSLocalizedString("Your request \"\(title)\" will appear on Shoutit soon.", comment:"")
+            } else {
+                descriptionlabel.text = NSLocalizedString("Your request will appear on Shoutit soon.", comment:"")
+            }
         }
     }
 }
