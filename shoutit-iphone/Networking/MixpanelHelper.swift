c74082d8727e8a9ac0bfb13f1864a1fba0fe9d49
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-04-18 18:20:03 +0200

    add is_guest flag on mixpanel params

--- shoutit-iphone/Networking/MixpanelHelper.swift
+++ shoutit-iphone/Networking/MixpanelHelper.swift
@@ -28,8 +28,13 @@ class MixpanelHelper {
     }
     
     private static var actionProperties: [String : AnyObject] {
-        return ["api_client" : "shoutit-ios",
-                "signed_user" : Account.sharedInstance.authData != nil]
+        var p: [String : AnyObject] = [:]
+        p["api_client"] = "shoutit-ios"
+        p["signed_user"] = Account.sharedInstance.authData != nil
+        if let user = Account.sharedInstance.user {
+            p["is_guest"] = user.isGuest
+        }
+        return p
     }
     
     // MARK: - Interface
