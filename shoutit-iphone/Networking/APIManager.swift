794d5ac9e7fed11ac3951997a61c5fa79fbff03a
Piotr Bernad <piotrbernadd@gmail.com>
2016-04-21 18:04:25 +0200

    Address Fix

--- shoutit-iphone/Networking/APIManager.swift
+++ shoutit-iphone/Networking/APIManager.swift
@@ -16,7 +16,7 @@ final class APIManager {
     
     // base url
     #if STAGING
-    static let baseURL = "https://dev.api.shoutit.com/v3/"
+    static let baseURL = "https://dev.api.shoutit.com/v3"
     // runscope url   "https://dev-api-shoutit-com-qm7w6bwy42b2.runscope.net/v3"
     // base dev url "http://dev.api.shoutit.com/v2"
     #else
