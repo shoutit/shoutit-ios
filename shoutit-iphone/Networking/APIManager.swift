4a0c78e338d99b272a2d6f72d34afa045257874a
Piotr Bernad <piotrbernadd@gmail.com>
2016-04-21 17:32:16 +0200

    Dev Base URL Instead of Runscope

--- shoutit-iphone/Networking/APIManager.swift
+++ shoutit-iphone/Networking/APIManager.swift
@@ -16,7 +16,9 @@ final class APIManager {
     
     // base url
     #if STAGING
-    static let baseURL = "https://dev-api-shoutit-com-qm7w6bwy42b2.runscope.net/v3" // base dev url "http://dev.api.shoutit.com/v2"
+    static let baseURL = "https://dev.api.shoutit.com/v3/"
+    // runscope url   "https://dev-api-shoutit-com-qm7w6bwy42b2.runscope.net/v3"
+    // base dev url "http://dev.api.shoutit.com/v2"
     #else
     static let baseURL = "https://api.shoutit.com/v3"
     #endif
