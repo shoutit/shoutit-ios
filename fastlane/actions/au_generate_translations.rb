4b45272a210e351ace537d5a439575aa97309ed5
piotrbernad <piotr.bernad@appunite.com>
2016-04-28 12:13:59 +0200

    Translations Action Imp

--- fastlane/actions/au_generate_translations.rb
+++ fastlane/actions/au_generate_translations.rb
@@ -6,26 +6,26 @@ module Fastlane
         
         Helper.log.info "Generating localization for project #{params[:project_file_path]}"
         
+        command = "cd #{params[:export_dir]}"
+        Fastlane::Actions.sh command, log: true
+        
+        command = "git reset HEAD --hard && git pull origin master"
+        Fastlane::Actions.sh command, log: true
+        
+        command = "cd .."
+        Fastlane::Actions.sh command, log: true
+        
         params[:languages].strip.split(" ").each do |lang|
           Helper.log.info "Generating localization file for: #{lang}"
-          
-          command = "cd #{params[:export_dir]}"
-          Fastlane::Actions.sh command, log: true
-          
-          command = "git reset HEAD --hard && git pull origin master"
-          Fastlane::Actions.sh command, log: true
-          
-          command = "cd .."
-          Fastlane::Actions.sh command, log: true
-          
+        
           command = "xcodebuild -exportLocalizations -localizationPath #{params[:export_dir]} -project #{params[:project_file_path]} -exportLanguage #{lang}"
-          Fastlane::Actions.sh command, log: true
-          
-          command = "cd #{params[:export_dir]} && git add -A && git commit -am 'translations update' && git push origin HEAD:master"
           Fastlane::Actions.sh command, log: true
           
         end
 
+        command = "cd #{params[:export_dir]} && git add -A && git commit -am 'translations update' && git push origin HEAD:master"
+        Fastlane::Actions.sh command, log: true
+        
         Helper.log.info "Localization files exported to #{params[:export_dir]}"
 
       end
