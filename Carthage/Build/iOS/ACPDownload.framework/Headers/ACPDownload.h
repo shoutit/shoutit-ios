0b3f399ed88aaf223e10357252a26617b0bf4ac3
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-06-14 17:58:37 +0200

    track built frameworks

---
+++ Carthage/Build/iOS/ACPDownload.framework/Headers/ACPDownload.h
@@ -0,0 +1,27 @@
+//
+//  ACPDownload iOS.h
+//  ACPDownload iOS
+//
+//  Created by Piotr Bernad on 08/06/16.
+//  Copyright © 2016 Antonio Casero Palmero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACPDownload iOS.
+FOUNDATION_EXPORT double ACPDownload_iOSVersionNumber;
+
+//! Project version string for ACPDownload iOS.
+FOUNDATION_EXPORT const unsigned char ACPDownload_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACPDownload_iOS/PublicHeader.h>
+
+#import <ACPDownload/ACPStaticImages.h>
+#import <ACPDownload/ACPStaticImagesAlternative.h>
+#import <ACPDownload/ACPIndeterminateGoogleLayer.h>
+#import <ACPDownload/ACPIndeterminateLayer.h>
+#import <ACPDownload/ACPProgressLayer.h>
+#import <ACPDownload/ACPLayerProtocol.h>
+#import <ACPDownload/ACPStaticImagesProtocol.h>
+#import <ACPDownload/ACPDownloadConstants.h>
+#import <ACPDownload/ACPDownloadView.h>
